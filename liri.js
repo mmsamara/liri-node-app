5e89c74e639b6cfe819d8464aee16bd3d9471366
mmsamara <mmsamara@ncsu.edu>
2018-03-30 08:50:09 -0400

    n

--- liri.js
+++ liri.js
@@ -2,3 +2,23 @@ require("dotenv").config();
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var argument = process.argv[2];
+
+switch(argument) {
+	case "my-tweets":
+
+		break;
+	case "spotify-this-song":
+
+		break;
+	case "movie-this":
+
+		break;
+	case "do-what-it-says":
+
+		break;
+	default:
+		console.log("Enter a valid command");
+
+}
